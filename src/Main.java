f0d565a817d6a1646f163e4c4089de4c34dabf68
WhoReadThisWillDie <msadkoff@mail.ru>
2022-10-22 23:22:42 +0300

    вроде сделал

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        byte[] a = new byte[n / 8 + 1];
+        for (int i = 0; i < n; ++i) {
+            byte x = (byte) (in.nextBoolean() ? 1 : 0);
+            a[i / 8] = (byte) (a[i / 8] | (x << (i % 8)));
+        }
+        for (int i = 0; i < n; ++i) {
+            System.out.print(((a[i / 8] & (1 << (i % 8))) != 0) + " ");
+        }
     }
 }

bd6d08f90bd2256418d0dcd7aabfc413d685d2d6
WhoReadThisWillDie <msadkoff@mail.ru>
2022-11-07 22:31:55 +0300

    Done!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Fraction fraction4 = new Fraction(0.05);
+        System.out.println(fraction4);
     }
 }

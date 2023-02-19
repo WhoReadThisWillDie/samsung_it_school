5ea67b289aa9d7b50636bc6e8f1ddff2b11294be
WhoReadThisWillDie <msadkoff@mail.ru>
2023-02-20 00:25:24 +0300

    Это всё НАШЕ!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+
 public class Main {
+
+    public static <T extends Comparable<T>> int binarySearch(T[] a, T key, int low, int hi) {
+        if (low > hi) throw new IllegalArgumentException("Low > hi!");
+        if (low < 0 || hi >= a.length) throw new IllegalArgumentException("Incorrect indexes");
+        int mid = 0;
+        while (low <= hi) {
+            mid = (low + hi) / 2;
+            T value = a[mid];
+            if (value.compareTo(key) == 0)
+                return mid;
+            else if (value.compareTo(key) < 0)
+                low = mid + 1;
+            else
+                hi = mid - 1;
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        MyArrayList<String> a = new MyArrayList<>();
+//        a.add("1");
+//        a.add("2");
+//        a.add("3");
+//        MyArrayList<String> b = new MyArrayList<>();
+//        b.add("4");
+//        b.addAll(a);
+//        System.out.println(b.contains("9"));
+//
+//        for (String s : b) {
+//            System.out.println(s);
+//        }
     }
 }
